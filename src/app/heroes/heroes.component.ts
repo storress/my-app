f7af378ca013e723e6f8d328c9ed5d399d9a983c
Salomon Torres <nomolas1@gmail.com>
2017-11-13 13:11:29 -0300

    Services done, need fix css

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES} from '../mock-heroes';
+import { HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,14 +12,21 @@ export class HeroesComponent implements OnInit {
 
   selectedHero: Hero;
 
-  heroes = HEROES;
+  heroes: Hero[];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
 }
