6362040ab0a6dd0a0db43fd5fb9093d6a119a0cc
Salomon Torres <nomolas1@gmail.com>
2017-11-14 11:24:11 -0300

    Routing done

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,15 +4,21 @@ import { Observable} from 'rxjs/Observable';
 import { of} from 'rxjs/observable/of';
 
 import { Hero } from './hero';
-import { HEROES} from './mock-heroes';
-import { MessageService} from './message.service';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
 
-  getHeroes(): Observable<Hero[]>{
+  getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // Todo: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 
   constructor(private messageService: MessageService) { }
